dc8d35ed98c459de6c62ab6514ac347631a35967
Hoge <hoge@hoge>
2023-06-12 09:16:52 +0900

    画像クリックに対応

--- script.js
+++ script.js
@@ -1,9 +1,58 @@
+const originalWidth = 1669;
+const originalHeight = 660;
+
+const keyRegions = {
+  'q': [221, 210, 327, 311],
+  'w': [327, 210, 433, 311],
+  'e': [433, 210, 539, 311],
+  'r': [539, 210, 645, 311],
+  't': [645, 210, 751, 311],
+  'y': [751, 210, 857, 311],
+  'u': [857, 210, 963, 311],
+  'i': [963, 210, 1069, 311],
+  'o': [1069, 210, 1175, 311],
+  
+  'a': [240, 311, 346, 416],
+  's': [346, 311, 452, 416],
+  'd': [452, 311, 558, 416],
+  'f': [558, 311, 664, 416],
+  'g': [664, 311, 770, 416],
+  'h': [770, 311, 876, 416],
+  'j': [876, 311, 982, 416],
+  'k': [982, 311, 1088, 416],
+  'l': [1088, 311, 1194, 416],
+  ';': [1194, 311, 1300, 416],
+  
+  'v': [585, 416, 692, 524],
+  'n': [797, 416, 905, 524],
+};
+
+const keyboard = document.getElementById('keyboard');
+keyboard.addEventListener('click', function(event) {
+  const scaleX = originalWidth / keyboard.offsetWidth;
+  const scaleY = originalHeight / keyboard.offsetHeight;
+  const x = event.offsetX * scaleX;
+  const y = event.offsetY * scaleY;
+  
+  for (const key in keyRegions) {
+    const [x1, y1, x2, y2] = keyRegions[key];
+    if (x >= x1 && x < x2 && y >= y1 && y < y2) {
+      displayImageForKey(key);
+      break;
+    }
+  }
+});
+
 document.addEventListener('keydown', function(event) {
+  displayImageForKey(event.key);
+});
+
+function displayImageForKey(key){
   const container = document.getElementById('image-container');
   container.innerHTML = '';
   
   let img = document.createElement('img');
-  switch(event.key) {
+  switch(key) {
     case 't':
       img.src = 'img/start1.png';
       break;
@@ -75,4 +124,4 @@ document.addEventListener('keydown', function(event) {
   }
   
   container.appendChild(img);
-});
+}
